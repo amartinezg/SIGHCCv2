b4c8dbb902f8226d4a1a11a1faf97737969cfab1
Andres Martinez Gutierrez <amartinez0000@gmail.com>
2015-07-22 21:02:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SIGHCCv2::Application.config.secret_token = '64d8081f6d68193b1f28246d29a7cced2371acc1a42b4eb13fe8c2bc44f2e51635dfcd5101ddd413d1db868da76dbaf93fb66168cf57251ebe9500296e658af7'
